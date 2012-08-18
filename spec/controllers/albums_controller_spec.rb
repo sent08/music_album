86f39e59cd04cf0612b875ba424741b5c3b60dd8
sumeru <senthil.k@sumerusolutions.com>
2012-08-19 03:37:14 +0530

    Albums controller spec

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
+include ActionDispatch::TestProcess
 
 describe AlbumsController do
+  def valid_attributes
+    {
+      "title"=>"FirstAlbum",
+      "description"=>"This is my first album",
+      "album" => fixture_file_upload('/sample.mp3', 'audio/mpeg')
+    }
+  end
 
+  before(:each) do
+    activate_authlogic
+    create(:user)
+    UserSession.create mock_model(User, :id => 1)
+  end
+
+  describe "Get new" do
+    it"should return new album"do
+      get(:new)
+      assigns[:album].should be_a_new(Album)
+    end
+  end
 end
