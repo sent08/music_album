15a93705d66b9be14432b3acd8bef3336232ea30
sumeru <senthil.k@sumerusolutions.com>
2012-08-19 02:53:45 +0530

    Initial spec for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
+include ActionDispatch::TestProcess
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:albums) }
+
+  describe "#display_name" do
+    it{ create(:user)
+      should validate_presence_of(:display_name)}
+  end
+
+  describe "#first_name" do
+    it{should validate_presence_of(:first_name)}
+  end
+
+
+  describe "#last_name" do
+    it{should validate_presence_of(:last_name)}
+  end
+
+
+  describe "#email" do
+    it{should validate_presence_of(:email)}
+    it{should validate_uniqueness_of(:email)}
+  end
+
+
+  before(:all) do
+    @ram = create(:user,:first_name=>"ram")
+    @joe = create(:user,:first_name=>"joe")
+    @valid_user = create(:user,:first_name=>"validuser")
+  end
+
 end
