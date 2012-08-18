6c6920e184f9812d401d116bbd878134ea9c613a
sumeru <senthil.k@sumerusolutions.com>
2012-08-19 02:47:06 +0530

    Defined association and authlogic for user session

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,13 @@
 class Album < ActiveRecord::Base
+  belongs_to :user
+
+  has_attached_file :album_cover,
+    :path => ":rails_root/public/album_covers/:id/:styles.:extension",
+    :url => "/album_covers/:id/:style/:basename.:extension",
+    :styles => {
+    :thumb=> "100x100#",
+    :small  => "150x150>",
+    :origial => "300X300"
+  }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ActiveRecord::Base
+
+  acts_as_authentic
+  attr_accessible :email, :password, :password_confirmation, :first_name, :last_name, :display_name
+
+  #Validations
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :display_name, :presence => true
+  validates :email, :presence => true, :uniqueness => true
+  validates_format_of :email,
+    :with => %r{^(?:[_a-z0-9-]+)(\.[_a-z0-9-]+)*@([a-z0-9-]+)(\.[a-zA-Z0-9\-\.]+)*(\.[a-z]{2,4})$}i,
+    :message => "is not a valid email address.",
+    :if => Proc.new {|u| not u.email.blank?}
+  validates :password, :presence => true
+  validates :password_confirmation, :presence => true
+  validates_confirmation_of :password
+
+  #Associations
+  has_many :albums, :dependent => :destroy
+
 end
