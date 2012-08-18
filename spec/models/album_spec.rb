4802b9b36c1a1cdf84a6e2080678f4ae6bcc61a5
sumeru <senthil.k@sumerusolutions.com>
2012-08-19 03:07:06 +0530

    Album Spec
    Finished in 0.33802 seconds
    ←[32m6 examples, 0 failures←[0m

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,38 @@
 require 'spec_helper'
+include ActionDispatch::TestProcess
 
 describe Album do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should belong_to(:user) }
+
+  describe "#title" do
+    it{should validate_presence_of(:title)}
+  end
+
+  before(:all) do
+    @valid_user = create(:user,:first_name=>"validuser")
+    @new_album = create(:album, :user => @valid_user)
+  end
+
+  it "create valid album" do
+    count = Album.count
+    @album = create(:album, :user => @valid_user)
+    Album.all.should have(count.to_i + 1).items
+  end
+
+  it "will not create album without title" do
+    @new_album.title = nil
+    @new_album.should have(1).error_on(:title)
+  end
+
+  it "will not create album without album file" do
+    @new_album.album = nil
+    @new_album.should have(1).error_on(:album)
+  end
+
+  it "will not create album without user" do
+    @new_album.user = nil
+    @new_album.should have(1).error_on(:user)
+  end
+
 end
